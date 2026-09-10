f8c18b35eb816168a9be0dbd5b5f9ff188c6fcd9
natfresco-DK <132372727+natfresco-DK@users.noreply.github.com>
2026-09-10 08:52:06 +0200

    Fix test assertion to demonstrate a passing CI pipeline

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
+//    @Test
+//    void shouldFailOnPurpose() {
+//        assertEquals(5, 2 + 2, "This test is intentionally failing to demonstrate a red CI pipeline.");
+//    }
+
     @Test
     void shouldFailOnPurpose() {
-        assertEquals(5, 2 + 2, "This test is intentionally failing to demonstrate a red CI pipeline.");
+        assertEquals(4, 2 + 2, "This test is intentionally succeed to demonstrate a red CI pipeline.");
     }
 }
